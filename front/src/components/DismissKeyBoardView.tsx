11797212fef1944cf4f9fc7149e53e787fcefb33
cy3589 <jcy3589@gmail.com>
2022-06-18 22:07:19 +0900

    HOC

--- front/src/components/DismissKeyBoardView.tsx
+++ front/src/components/DismissKeyBoardView.tsx
@@ -7,15 +7,22 @@ import {
 } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
-const DismissKeyboardView: React.FC<{
-  children: React.ReactNode;
-  style?: ViewStyle;
-}> = ({children, ...props}) => (
-  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <KeyboardAwareScrollView {...props} style={props.style}>
-      {children}
-    </KeyboardAwareScrollView>
-  </TouchableWithoutFeedback>
-);
+const DismissKeyboardHOC =
+  (
+    Comp: typeof KeyboardAwareScrollView,
+  ): React.FC<{
+    children: React.ReactNode;
+    style?: StyleProp<ViewStyle>;
+  }> =>
+  ({children, ...props}) =>
+    (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <Comp {...props} style={props.style}>
+          {children}
+        </Comp>
+      </TouchableWithoutFeedback>
+    );
+
+const DismissKeyboardView = DismissKeyboardHOC(KeyboardAwareScrollView);
 
 export default DismissKeyboardView;
