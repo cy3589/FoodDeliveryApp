3f1ecf41a8ad9e421d38023e7529471184359711
cy3589 <jcy3589@gmail.com>
2022-06-18 17:12:19 +0900

    lint setting

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,8 +1,46 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'react/jsx-filename-extension': [
+      2,
+      {extensions: ['.js', '.jsx', '.ts', '.tsx']},
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {js: 'never', jsx: 'never', ts: 'never', tsx: 'never', json: 'never'},
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'prettier/prettier': ['error'],
+    'react/jsx-props-no-spreading': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    'no-use-before-define': [
+      'error',
+      {functions: true, classes: true, variables: false},
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+      node: {extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']},
+    },
   },
 };
